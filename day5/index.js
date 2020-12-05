9697f0df100eb7304da60d45828b2533fbca08d9
Joe Abell <joe.abell@isotoma.com>
2020-12-05 15:12:25 +0000

    refactor day 5

--- day5/index.js
+++ day5/index.js
@@ -1,20 +1,17 @@
 const fs = require('fs');
 const rawData = fs.readFileSync('./input.txt', 'utf8');
 
-const passes = rawData.split('\n');
-const passesBinary = passes
-    .map(pass => parseInt(pass
-        .split('F').join('0')
+const passesBin = rawData.split('\n');
+const passesDec = passesBin.map(pass => parseInt(
+    pass.split('F').join('0')
         .split('B').join('1')
         .split('L').join('0')
-        .split('R').join('1'), 2))
-    .sort((a, b) => a - b)
+        .split('R').join('1'),
+    2
+)).sort((a, b) => a - b)
 
-const highestPass = passesBinary[passesBinary.length - 1]
-const lowestPass = passesBinary[0];
-const missingPass = passesBinary
-    .find((pass, passIndex) => { // Finds the pass after the missing pass
-        return (pass - lowestPass !== passIndex)
-    }) - 1; // - 1 to find the pass number that is missing
-
+const highestPass = passesDec[passesDec.length - 1]
+const lowestPass = passesDec[0];
+const passAfterMissingPass = passesDec.find((pass, passIndex) => (pass - lowestPass !== passIndex))
+const missingPass = passAfterMissingPass - 1;
 console.log(highestPass, missingPass);
