be7dfbb6d8edf9881b286e88f29fbc62916daba3
Joe Abell <joe.abell@isotoma.com>
2020-12-06 10:25:28 +0000

    refactor

--- day6/index.js
+++ day6/index.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
+const { pipe, add } = require('ramda');
+
 const rawData = fs.readFileSync('./input.txt', 'utf8');
 const groups = rawData.split('\n\n');
 
-const add = (a, b) => a + b;
+const getPeople = group => group.split('\n');
+const getDuplicatedLetters = people => people.join('').split('');
+const dedupeLetters = duplicatedLetters => [...new Set(duplicatedLetters)];
+const getLettersSharedByEveryone = (people, letters) => letters.filter(letter => people.every(person => person.includes(letter)))
+
 const anyCount = groups
-    .map(group => {
-        const duplicatedLetters = group.split('\n').join('').split('');
-        const letters = [...new Set(duplicatedLetters)]
-        return letters.length;
-    })
+    .map(group => pipe(getPeople, getDuplicatedLetters, dedupeLetters)(group).length)
     .reduce(add, 0);
 
 const everyCount = groups.map(group => {
-    const people = group.split('\n');
-    const duplicatedLetters = people.join('').split('');
-    const letters = [...new Set(duplicatedLetters)]
-    const lettersEveryoneHas = letters.filter(letter => people.every(person => person.includes(letter)))
-    return lettersEveryoneHas.length;
+    const people = getPeople(group);
+    const letters = pipe(getDuplicatedLetters, dedupeLetters)(people);
+    return getLettersSharedByEveryone(people, letters).length;
 }).reduce(add, 0)
 
 console.log([anyCount, everyCount])
